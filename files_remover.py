92108b57681ba819c1735ef57e27873746ff03b0
Gumerov Artur <2diver7@gmail.com>
2022-10-16 00:29:53 +0400

    Функция доработана для вызова из терминала.

--- files_remover.py
+++ files_remover.py
@@ -1,11 +1,63 @@
-import re
 from logging import getLogger
-from os import listdir, path, remove, walk
+from os import getcwd, listdir, path, remove, walk
 from os.path import isfile, join
-from re import compile
+from re import IGNORECASE, compile
 
 
 class FileRemover:
+    @classmethod
+    def execute(cls):
+        files_types = 'jpg orf'
+        check_files_types = input(
+            f'Форматы файлов для удаления: {files_types}\n'
+            'Верно - y,\n'
+            'Неверно - <тип отфильтрованных файлов> '
+            '<тип неотфильтрованных файлов>\n'
+        )
+        if check_files_types != 'y':
+            if len(check_files_types.split(' ')) == 2:
+                files_types = check_files_types
+                print(f'Новые типы файлов: {files_types}')
+            else:
+                print('Необходимо ввести два формата через пробел')
+                exit(-1)
+
+        origin_files = getcwd()
+        check_origin = input(
+            f'Директория с отфильтрованными файлами: {origin_files}\n'
+            'Верно - y,\nНеверно - <путь к директории>\n'
+        )
+        if check_origin != 'y':
+            if path.isdir(check_origin):
+                origin_files = check_origin
+                print(
+                    f'Новая директория с отфильтрованными '
+                    f'файлами: {check_origin}'
+                )
+            else:
+                print('Указанный путь не является директорией')
+                exit(-1)
+
+        files_to_remove = origin_files.lower().replace('jpeg', 'raw')
+        check_target = input(
+            f'Директория с неотфильтрованными файлами: {files_to_remove}\n'
+            'Верно - y,\nНеверно - <путь к директории>\n'
+        )
+        if check_target != 'y':
+            if path.isdir(check_target):
+                files_to_remove = check_target
+                print(
+                    f'Новая директория с неотфильтрованными '
+                    f'файлами: {check_target}'
+                )
+            else:
+                print('Указанный путь не является директорией')
+                exit(-1)
+
+        cls.files_remover(
+            origin_files, files_to_remove, *files_types.split(' ')
+        )
+
     @classmethod
     def get_files_list(cls, src_path: str) -> list[str]:
         """Walk through path folder and clean files names from type."""
@@ -16,12 +68,20 @@ class FileRemover:
         return [f for f in listdir(src_path) if isfile(join(src_path, f))]
 
     @classmethod
-    def extension_cleaner(cls, files_list: list[str], file_type: str) -> list[str]:
-        type_pat = compile('\.' + file_type + '$', flags=re.IGNORECASE)
-        return [type_pat.sub('', f_name) for f_name in files_list if type_pat.search(f_name)]
+    def extension_cleaner(
+            cls, files_list: list[str], file_type: str
+    ) -> list[str]:
+        type_pat = compile(r'\.' + file_type + r'$', flags=IGNORECASE)
+        return [
+            type_pat.sub('', f_name)
+            for f_name in files_list
+            if type_pat.search(f_name)
+        ]
 
     @classmethod
-    def list_difference(cls, files_to_remove: list[str], files_to_exclude: list[str]) -> set[str]:
+    def list_difference(
+            cls, files_to_remove: list[str], files_to_exclude: list[str]
+    ) -> set[str]:
         return set(files_to_remove) - set(files_to_exclude)
 
     @classmethod
@@ -29,10 +89,16 @@ class FileRemover:
                       origin_ext: str = None, to_remove_ext: str = None) -> None:
         origin_list = cls.get_files_list(origin_dir)
         remove_list = cls.get_files_list(to_remove_dir)
-        diff = cls.list_difference(cls.extension_cleaner(remove_list, to_remove_ext),
-                                   cls.extension_cleaner(origin_list, origin_ext))
-        print(f'Подтвердите удаление {len(diff)} файлов из папки {to_remove_dir}')
-        if input() != 'y':
+        diff = cls.list_difference(
+            cls.extension_cleaner(remove_list, to_remove_ext),
+            cls.extension_cleaner(origin_list, origin_ext)
+        )
+
+        need_removing = input(
+                f'Подтвердите удаление {len(diff)} '
+                f'файлов из папки {to_remove_dir}\n'
+        )
+        if need_removing != 'y':
             print('Отмена удаления')
             return
         for f_name in diff:
@@ -54,8 +120,4 @@ class FileRemover:
         print(total_size)
 
 
-if __name__ == '__main__':
-    origin_files = r'/Users/a.gumerov/Pictures/JPEG/2022 Гелиос'
-    files_to_remove = r'/Users/a.gumerov/Pictures/RAW/2022 Гелиос'
-    FileRemover.files_remover(origin_files, files_to_remove, 'jpg', 'orf')
-    print('Done')
+FileRemover.execute()
